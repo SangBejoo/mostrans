a43be884c82cbca443adbd2e6568fd9d27757a51
SangBejoo <ayub.subagiya.tik21@mhsw.pnj.ac.id>
2024-09-06 13:56:58 +0700

    kelima

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-// src/App.js
 import React, { Suspense, lazy } from 'react';
-import { Route, Routes, useLocation, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
 import './App.css';
 
 const CharactersByLocation = lazy(() => import('./CharactersByLocation'));
 const CharacterDetail = lazy(() => import('./CharacterDetail'));
 const CharacterList = lazy(() => import('./CharacterList'));
-const LandingPage = lazy(() => import('./LandingPage'));
 
 const App = () => {
-    const location = useLocation();
-    const isLandingPage = location.pathname === '/';
-
     return (
         <div className="App">
-            {!isLandingPage && <Navbar />}
+            <Navbar />
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path="/character-list" element={<CharacterList />} />
                     <Route path="/location" element={<CharactersByLocation />} />
                     <Route path="/character/:id" element={<CharacterDetail />} />
-                    <Route path="/" exact element={<LandingPage />} />
                 </Routes>
             </Suspense>
         </div>
@@ -29,7 +23,7 @@ const App = () => {
 };
 
 const AppWrapper = () => (
-    <Router>
+    <Router basename="/mostrans">
         <App />
     </Router>
 );
