464f8eb0947478e5b4d4ce8b533ff690640f4b97
SangBejoo <ayub.subagiya.tik21@mhsw.pnj.ac.id>
2024-09-06 13:51:03 +0700

    keempat

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,10 +1,11 @@
-// src/LandingPage.js
 import React from 'react';
+import Navbar from './Navbar';
 import './LandingPage.css';
 
 const LandingPage = () => {
     return (
         <div className="landing-page">
+            <Navbar />
             <section className="showcase">
                 <div className="content">
                     <h1>Ayub Subagiya</h1>
